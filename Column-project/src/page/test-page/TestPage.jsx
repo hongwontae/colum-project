c49953ecbab6f9862f89f9348fedec1b4571f6f0
dnjsxoghd <feelchok1234@gmail.com>
2025-04-15 19:27:01 +0900

    quill

--- Column-project/src/page/test-page/TestPage.jsx
+++ Column-project/src/page/test-page/TestPage.jsx
@@ -1,13 +1,16 @@
 import { useEffect, useRef, useState } from "react";
-import ReactQuill from "react-quill";
+import ReactQuill, { Quill } from "react-quill";
 import "react-quill/dist/quill.snow.css";
+import ImageResize from "quill-image-resize-module-react";
+Quill.register("modules/imageResize", ImageResize);
 
 function TestPage() {
   const quillRef = useRef(null);
-  const [editorHtml, setEditorHtml] = useState("");
   const [imageFiles, setImageFiles] = useState([]);
+  const [editorHtml, setEditorHtml] = useState('')
 
   console.log(imageFiles);
+  console.log(editorHtml)
 
   useEffect(() => {
     if (quillRef.current) {
@@ -25,10 +28,17 @@ function TestPage() {
           const file = input.files[0];
           if (file) {
             const reader = new FileReader();
+
             reader.onload = () => {
+
+              const fileId = `${file.name}_${file.size}_${file.lastModified}`;
+              file._id = fileId;
+
               const base64 = reader.result;
               const range = quill.getSelection(true);
-              quill.insertEmbed(range.index, "image", base64);
+              const imageTag = `<img src="${base64}" data-id="${fileId}" />`;
+              quill.clipboard.dangerouslyPasteHTML(range.index, imageTag);
+              // 각 파일마다 고유한 id를 추가함
               setImageFiles((prev) => [...prev, file]);
             };
             reader.readAsDataURL(file);
@@ -38,9 +48,33 @@ function TestPage() {
     }
   }, []);
 
-  function handleChange(value) {
-    setEditorHtml(value);
-  }
+  useEffect(()=>{
+    if(quillRef.current){
+      const quill = quillRef.current.getEditor();
+
+      quill.clipboard.addMatcher('img', (node, delta)=>{
+        const dataId = node.getAttributes('data-id');
+      })
+
+      quill.on('text-change', ()=>{
+        const html = quill.root.innerHTML;
+        console.log(html)
+        setEditorHtml(html);
+
+        const currentImageIds = Array.from(quill.root.querySelectorAll('img'))
+        console.log(currentImageIds[0].getAttribute('data-id'))
+
+        // currentImageIds ? setImageFiles((prev)=>{
+        //   return prev.filter(file => {
+        //     return currentImageIds.includes(file._id);
+        //   })
+        // }) : null
+
+      })
+    }
+  }, [])
+
+
 
   return (
     <>
@@ -48,7 +82,7 @@ function TestPage() {
         <ReactQuill
           ref={quillRef}
           value={editorHtml}
-          onChange={handleChange}
+          className="text-black bg-white rounded-lg h-[20rem]"
           modules={{
             toolbar: [
               [{ header: [1, 2, false] }],
